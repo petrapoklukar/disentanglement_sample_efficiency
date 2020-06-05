305ea0aaff61dd865a9931a040e715962dfc60da
petrapoklukar <petra.poklukar@gmail.com>
2020-06-05 20:55:16 +0200

    Create task splits

---
+++ sample_efficiency/create_task_split.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 20:32:58 2020
+
+@author: petrapoklukar
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import sys
+sys.path.append('..')
+from disentanglement_lib.preprocessing import preprocess
+
+preproces_task_gin_bindings = [
+        "preprocess.preprocess_fn = @split_train_and_validation",
+        "preprocess.random_seed = 0",
+        "split_train_and_validation.unit_labels = True"
+]
+
+preprocess.preprocess_with_gin('3dshapes_task_s1000',
+                               overwrite=False,
+                               gin_config_files=None,
+                               gin_bindings=preproces_task_gin_bindings)
