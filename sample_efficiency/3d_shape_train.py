22b125a13d7a62c338dd687e046df6ba46091280
MWelle77 <mwelle@kth.se>
2020-06-05 16:56:45 +0200

    eval

--- sample_efficiency/3d_shape_train.py
+++ sample_efficiency/3d_shape_train.py
@@ -42,6 +42,7 @@ from disentanglement_lib.methods.unsupervised import train_partial
 from disentanglement_lib.methods.unsupervised import vae
 from disentanglement_lib.postprocessing import postprocess
 from disentanglement_lib.utils import aggregate_results
+from disentanglement_lib.visualize import visualize_model
 import tensorflow as tf
 import gin.tf
 
@@ -59,7 +60,7 @@ overwrite = True
 
 # We save the results in a `vae` subfolder.
 path_vae = os.path.join(base_path, "vae")
-#train_partial.train_with_gin(os.path.join(path_vae, "model"), overwrite, ["3d_shape_vae.gin"])
+train_partial.train_with_gin(os.path.join(path_vae, "model"), overwrite, ["3d_shape_vae.gin"])
 
 # The main training protocol of disentanglement_lib is defined in the
 # disentanglement_lib.methods.unsupervised.train module. To configure
@@ -71,21 +72,19 @@ path_vae = os.path.join(base_path, "vae")
 
 # 1b. Train beta VAE
 path_bvae = os.path.join(base_path, "bvae")
-#train_partial.train_with_gin(os.path.join(path_bvae, "model"), overwrite, ["3d_shape_bvae.gin"])
+train_partial.train_with_gin(os.path.join(path_bvae, "model"), overwrite, ["3d_shape_bvae.gin"])
 
 # 1c. Train Factor VAE
 #path_fvae = os.path.join(base_path, "fvae")
-#train_partial.train_with_gin(os.path.join(path_fvae, "model"), overwrite, ["3d_shape_fvae.gin"])
+train_partial.train_with_gin(os.path.join(path_fvae, "model"), overwrite, ["3d_shape_fvae.gin"])
 
 # 1c. Train Beta TC VAE
 #path_btcvae = os.path.join(base_path, "btcvae")
-#train_partial.train_with_gin(os.path.join(path_btcvae, "model"), overwrite, ["3d_shape_btcvae.gin"])
+train_partial.train_with_gin(os.path.join(path_btcvae, "model"), overwrite, ["3d_shape_btcvae.gin"])
 
 # 1d. Train Anneal VAE
 #path_annvae = os.path.join(base_path, "annvae")
-#train_partial.train_with_gin(os.path.join(path_annvae, "model"), overwrite, ["3d_shape_annvae.gin"])
-
-
+train_partial.train_with_gin(os.path.join(path_annvae, "model"), overwrite, ["3d_shape_annvae.gin"])
 
 
 # 3. Extract the mean representation for both of these models.
@@ -113,94 +112,101 @@ for path in [path_vae, path_bvae]:
 # disentanglement_lib.evaluation.evaluate.metrics and the predefined .gin
 # configuration files in
 # disentanglement_lib/config/unsupervised_study_v1/metrics_configs/(...).
-gin_bindings = [
-    "evaluation.evaluation_fn = @mig",
-    "dataset.name='3dshapes'",
-    "evaluation.random_seed = 0",
-    "mig.num_train=1000",
-    "discretizer.discretizer_fn = @histogram_discretizer",
-    "discretizer.num_bins = 20"
-]
+# gin_bindings = [
+#     "evaluation.evaluation_fn = @mig",
+#     #"dataset.name='auto'",
+#     "dataset.name='3dshapes'",
+#     "evaluation.random_seed = 0",
+#     "mig.num_train=1000",
+#     "discretizer.discretizer_fn = @histogram_discretizer",
+#     "discretizer.num_bins = 20"
+# ]
+# for path in [path_vae, path_bvae]:
+#   result_path = os.path.join(path, "metrics", "mig")
+#   representation_path = os.path.join(path, "representation")
+#   evaluate.evaluate_with_gin(
+#       representation_path, result_path, overwrite, gin_bindings=gin_bindings)
+
+
+
+# # 4. beta_vae_sklearn
+# gin_bindings = [
+#     "evaluation.evaluation_fn = @beta_vae_sklearn",
+#     "dataset.name='3dshapes'",
+#     "evaluation.random_seed = 0",
+#     "beta_vae_sklearn.batch_size=16",
+#     "beta_vae_sklearn.num_train=1000",
+#     "beta_vae_sklearn.num_eval=10000",
+#     "discretizer.discretizer_fn = @histogram_discretizer",
+#     "discretizer.num_bins = 20"
+# ]
+# for path in [path_vae, path_bvae]:
+#   result_path = os.path.join(path, "metrics", "bvae")
+#   representation_path = os.path.join(path, "representation")
+#   evaluate.evaluate_with_gin(
+#       representation_path, result_path, overwrite, gin_bindings=gin_bindings)
+
+
+# 5. Factor VAE dis
+# gin_bindings = [
+#     "evaluation.evaluation_fn = @factor_vae_score",
+#     "dataset.name='3dshapes'",
+#     "evaluation.random_seed = 0",
+#     "factor_vae_score.batch_size=16",
+#     "factor_vae_score.num_train=1000",
+#     "factor_vae_score.num_eval=1000",
+#     "factor_vae_score.num_variance_estimate=1000",
+#     "discretizer.discretizer_fn = @histogram_discretizer",
+#     "discretizer.num_bins = 20"
+# ]
+# for path in [path_vae, path_bvae]:
+#   result_path = os.path.join(path, "metrics", "fvae")
+#   representation_path = os.path.join(path, "representation")
+#   evaluate.evaluate_with_gin(
+#       representation_path, result_path, overwrite, gin_bindings=gin_bindings)
+
+
+
+# # 6. DCI score
+# gin_bindings = [
+#     "evaluation.evaluation_fn = @dci",
+#     "dataset.name='3dshapes'",
+#     "evaluation.random_seed = 0",
+#     "dci.batch_size=16",
+#     "dci.num_train=1000",
+#     "dci.num_test=1000",
+#     "discretizer.discretizer_fn = @histogram_discretizer",
+#     "discretizer.num_bins = 20"
+# ]
+# for path in [path_vae, path_bvae]:
+#   result_path = os.path.join(path, "metrics", "dci")
+#   representation_path = os.path.join(path, "representation")
+#   evaluate.evaluate_with_gin(
+#       representation_path, result_path, overwrite, gin_bindings=gin_bindings)
+
+
+# # 6. Aggregate the results.
+# # ------------------------------------------------------------------------------
+# # In the previous steps, we saved the scores to several output directories. We
+# # can aggregate all the results using the following command.
+# pattern = os.path.join(base_path,
+#                        "*/metrics/*/results/aggregate/evaluation.json")
+# results_path = os.path.join(base_path, "results.json")
+# aggregate_results.aggregate_results_to_json(
+#     pattern, results_path)
+
+# # 7. Print out the final Pandas data frame with the results.
+# # ------------------------------------------------------------------------------
+# # The aggregated results contains for each computed metric all the configuration
+# # options and all the results captured in the steps along the pipeline. This
+# # should make it easy to analyze the experimental results in an interactive
+# # Python shell. At this point, note that the scores we computed in this example
+# # are not realistic as we only trained the models for a few steps and our custom
+# # metric always returns 1.
+# model_results = aggregate_results.load_aggregated_json_results(results_path)
+# print(model_results)
+
+
+#8 Visulisation
 for path in [path_vae, path_bvae]:
-  result_path = os.path.join(path, "metrics", "mig")
-  representation_path = os.path.join(path, "representation")
-  evaluate.evaluate_with_gin(
-      representation_path, result_path, overwrite, gin_bindings=gin_bindings)
-
-
-a=1/0
-# 5. Compute a custom disentanglement metric for both models.
-# ------------------------------------------------------------------------------
-# The following function implements a dummy metric. Note that all metrics get
-# ground_truth_data, representation_function, random_state arguments by the
-# evaluation protocol, while all other arguments have to be configured via gin.
-@gin.configurable(
-    "custom_metric",
-    blacklist=["ground_truth_data", "representation_function", "random_state"])
-def compute_custom_metric(ground_truth_data,
-                          representation_function,
-                          random_state,
-                          num_train=gin.REQUIRED,
-                          batch_size=16):
-  """Example of a custom (dummy) metric.
-
-  Preimplemented metrics can be found in disentanglement_lib.evaluation.metrics.
-
-  Args:
-    ground_truth_data: GroundTruthData to be sampled from.
-    representation_function: Function that takes observations as input and
-      outputs a dim_representation sized representation for each observation.
-    random_state: Numpy random state used for randomness.
-    num_train: Number of points used for training.
-    batch_size: Batch size for sampling.
-
-  Returns:
-    Dict with disentanglement score.
-  """
-  score_dict = {}
-
-  # This is how to obtain the representations of num_train points along with the
-  # ground-truth factors of variation.
-  representation, factors_of_variations = utils.generate_batch_factor_code(
-      ground_truth_data, representation_function, num_train, random_state,
-      batch_size)
-  # We could now compute a metric based on representation and
-  # factors_of_variations. However, for the sake of brevity, we just return 1.
-  del representation, factors_of_variations
-  score_dict["custom_metric"] = 1.
-  return score_dict
-
-
-# To compute the score, we again call the evaluation protocol with a gin
-# configuration. At this point, note that for all steps, we have to set a
-# random seed (in this case via `evaluation.random_seed`).
-gin_bindings = [
-    "evaluation.evaluation_fn = @custom_metric",
-    "custom_metric.num_train = 100", "evaluation.random_seed = 0",
-    "dataset.name='auto'"
-]
-for path in [path_vae, path_custom_vae]:
-  result_path = os.path.join(path, "metrics", "custom_metric")
-  evaluate.evaluate_with_gin(
-      representation_path, result_path, overwrite, gin_bindings=gin_bindings)
-
-# 6. Aggregate the results.
-# ------------------------------------------------------------------------------
-# In the previous steps, we saved the scores to several output directories. We
-# can aggregate all the results using the following command.
-pattern = os.path.join(base_path,
-                       "*/metrics/*/results/aggregate/evaluation.json")
-results_path = os.path.join(base_path, "results.json")
-aggregate_results.aggregate_results_to_json(
-    pattern, results_path)
-
-# 7. Print out the final Pandas data frame with the results.
-# ------------------------------------------------------------------------------
-# The aggregated results contains for each computed metric all the configuration
-# options and all the results captured in the steps along the pipeline. This
-# should make it easy to analyze the experimental results in an interactive
-# Python shell. At this point, note that the scores we computed in this example
-# are not realistic as we only trained the models for a few steps and our custom
-# metric always returns 1.
-model_results = aggregate_results.load_aggregated_json_results(results_path)
-print(model_results)
+  visualize_model.visualize(path, path+"/vis", overwrite)
