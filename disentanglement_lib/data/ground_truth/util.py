b349321ef59697ebf07b1ccb7c3210f7f4d041f4
petrapoklukar <petra.poklukar@gmail.com>
2020-06-09 20:15:15 +0200

    Added visualisation and random evaluation

--- disentanglement_lib/data/ground_truth/util.py
+++ disentanglement_lib/data/ground_truth/util.py
@@ -76,7 +76,7 @@ def tf_random_labeled_data_set_from_ground_truth_data(ground_truth_data,
       observation, _ = ground_truth_data.sample_observations_and_labels(1, random_state)
       print(representation_function(observation))
       print(representation_function(observation).shape)
-      representation_shape = np.prod(representation_function(observation).shape.as_list())
+      representation_shape = np.prod(representation_function(observation).shape)
       random_representation = locals()[random_fn](representation_shape)
       yield (random_representation[0], observation[0])
 
