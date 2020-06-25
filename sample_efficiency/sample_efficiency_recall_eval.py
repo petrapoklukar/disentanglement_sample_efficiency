650935df0edb7b106632d9d0e77f5a5a7db60924
petrapoklukar <petra.poklukar@gmail.com>
2020-06-25 18:56:56 +0200

    Aggregation over interventions

--- sample_efficiency/sample_efficiency_recall_eval.py
+++ sample_efficiency/sample_efficiency_recall_eval.py
@@ -16,6 +16,7 @@ from absl import flags
 import sys
 sys.path.append('..')
 from disentanglement_lib.evaluation.metrics import utils
+from disentanglement_lib.preprocessing import preprocess
 from disentanglement_lib.evaluation import evaluate_with_decodings
 
 FLAGS = flags.FLAGS
@@ -35,13 +36,27 @@ def main(unused_argv):
   vae_path = os.path.join(base_path, FLAGS.model)
   model_path = os.path.join(vae_path, "model")
   print(vae_path, model_path)
+  
+  print("\n\n*- Preprocessing '%s' \n\n" %(FLAGS.dataset))
+  preproces_gin_bindings = [
+            "dataset.name = '%s'" %(FLAGS.dataset),
+            "preprocess.preprocess_fn = @split_train_and_validation",
+            "split_train_and_validation.random_seed = %d" %(FLAGS.rng)
+      ]
+
+  preprocess.preprocess_with_gin(FLAGS.dataset,
+                                 FLAGS.model,
+                                 overwrite=FLAGS.overwrite,
+                                 gin_config_files=None,
+                                 gin_bindings=preproces_gin_bindings)
+  print("\n\n*- Preprocessing DONE \n\n")
 
   print("\n\n*- Evaluating Recall.")
   gin_bindings = [
       "evaluate_with_decodings.evaluation_fn = @recall",
       "evaluate_with_decodings.postprocess_fn = @mean_representation",
       "evaluate_with_decodings.random_seed = 0",
-      "dataset.name='3dshapes_model_s100'",
+      "dataset.name='3dshapes_model_s1000'",
       "recall.num_recall_samples = 100",
       "recall.nhood_sizes = [3, 5]",
       "recall.num_interventions_per_latent_dim = 5"
@@ -49,6 +64,9 @@ def main(unused_argv):
   result_path = os.path.join(vae_path, "metrics", "test_recall_100")
   evaluate_with_decodings.evaluate_with_gin(
       model_path, result_path, FLAGS.overwrite, gin_bindings=gin_bindings)
+  
+  preprocess.destroy_train_and_validation_splits(
+      FLAGS.dataset + '_' + FLAGS.model + '_' + str(FLAGS.rng))
   print("\n\n*- Evaluation COMPLETED \n\n")
 
 
