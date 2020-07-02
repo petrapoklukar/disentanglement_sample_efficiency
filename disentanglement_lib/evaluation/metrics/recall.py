cda0f054c748337a575720665980cd9cc27ab87e
petrapoklukar <petra.poklukar@gmail.com>
2020-07-02 16:08:59 +0200

    New recall measure

--- disentanglement_lib/evaluation/metrics/recall.py
+++ disentanglement_lib/evaluation/metrics/recall.py
@@ -50,10 +50,26 @@ def compute_recall(ground_truth_data,
   num_recall_samples = train_ground_truth_data.data_size
   dummy_input = ground_truth_data.sample_observations(1, random_state)
   dummy_mean, dummy_var = encoder_fn(dummy_input)
-  # Samples from the prior
+  
+  # Samples from the normal prior
   latent_dim = repr_transform_fn(*encoder_fn(dummy_input)).shape[-1]
   latent_shape = [num_recall_samples, latent_dim]
   latent_prior_samples_np = np.random.normal(size=latent_shape)
+  
+  # Ground truth samples 
+  gt_train_samples = ground_truth_data.sample_observations(num_recall_samples, random_state)
+  gt_train_repr = repr_transform_fn(*encoder_fn(gt_train_samples))
+  gt_train_repr_mean = np.mean(gt_train_repr, axis=0)
+  gt_train_repr_std = np.std(gt_train_repr, axis=0)
+  gt_train_repr_min = np.min(gt_train_repr, axis=0)
+  gt_train_repr_max = np.max(gt_train_repr, axis=0)
+  
+  print('\n\n\n ', gt_train_repr_mean.shape, gt_train_repr_std.shape)
+  
+  # The predetermined set of interventions from the estimated training prior
+  fixed_trained_prior_samples_np = np.random.normal(loc=gt_train_repr_mean, 
+                                              scale=gt_train_repr_std, 
+                                              size=latent_shape)
   
   result_d = {'nhoods': nhood_sizes}
   sess = tf.Session()
@@ -61,124 +77,194 @@ def compute_recall(ground_truth_data,
     n_comp = min(num_recall_samples, 1000)
 
     print('\n\n\n Computing the total recall...')
-    # Generated samples
-    generated_prior_samples = decoder_fn(latent_prior_samples_np)
-    generated_prior_samples = generated_prior_samples.reshape(num_recall_samples, -1)
-    generated_pca = PCA(n_components=n_comp)
-    reduced_generated_prior_samples = generated_pca.fit_transform(generated_prior_samples)
-    
-    # Sample ground truth data
-    gt_samples = ground_truth_data.sample_observations(num_recall_samples, random_state)
-    decoded_gt_samples = decoder_fn(repr_transform_fn(*encoder_fn(gt_samples)))
+    # Sample ground truth data and vae process it
+    decoded_gt_samples = decoder_fn(repr_transform_fn(*encoder_fn(gt_train_samples)))
     decoded_gt_samples = decoded_gt_samples.reshape(num_recall_samples, -1)
     decoded_gt_pca = PCA(n_components=n_comp)
     reduced_decoded_gt_samples = decoded_gt_pca.fit_transform(decoded_gt_samples)
+
+    # Generated samples from the normal prior, processed with gt PCA
+    generated_prior_samples = decoder_fn(latent_prior_samples_np)
+    generated_prior_samples = generated_prior_samples.reshape(num_recall_samples, -1)
+    reduced_generated_prior_samples = decoded_gt_pca.transform(generated_prior_samples)
     assert(reduced_generated_prior_samples.shape == reduced_decoded_gt_samples.shape)
-
-    gt_samples = gt_samples.reshape(num_recall_samples, -1)
-    gt_pca = PCA(n_components=n_comp)
-    reduced_gt_samples = gt_pca.fit_transform(gt_samples)
-    assert(reduced_gt_samples.shape == reduced_decoded_gt_samples.shape)
-
-    # compute model recall: gt vs generated
-    gt_generated_result = iprd.knn_precision_recall_features(
-        reduced_gt_samples, 
-        reduced_generated_prior_samples, 
-        nhood_sizes=nhood_sizes,
-        row_batch_size=500, col_batch_size=100, num_gpus=1)
-    update_result_dict(result_d, ['gt_generated_', gt_generated_result])
     
-    # compute model recall: model(gt) vs generated
-    decoded_gt_generated_result = iprd.knn_precision_recall_features(
+    # Generated samples from the estimated training prior, processed with gt PCA
+    generated_trained_prior_samples = decoder_fn(fixed_trained_prior_samples_np)
+    generated_trained_prior_samples = generated_trained_prior_samples.reshape(num_recall_samples, -1)
+    reduced_generated_trained_prior_samples = decoded_gt_pca.transform(generated_trained_prior_samples)
+    
+    # --- Original sharp images - discarded to now 
+#    gt_train_samples = gt_samples.reshape(num_recall_samples, -1)
+#    gt_pca = PCA(n_components=n_comp)
+#    reduced_gt_samples = gt_pca.fit_transform(gt_samples)
+#    assert(reduced_gt_samples.shape == reduced_decoded_gt_samples.shape)
+
+#    # compute model recall: gt vs generated
+#    gt_generated_result = iprd.knn_precision_recall_features(
+#        reduced_gt_samples, 
+#        reduced_generated_prior_samples, 
+#        nhood_sizes=nhood_sizes,
+#        row_batch_size=500, col_batch_size=100, num_gpus=1)
+#    update_result_dict(result_d, ['gt_generated_', gt_generated_result])
+    # ----
+    
+    # compute model recall: model(gt) vs normal prior generated
+    decoded_gt_prior_generated_result = iprd.knn_precision_recall_features(
         reduced_decoded_gt_samples, 
         reduced_generated_prior_samples, 
         nhood_sizes=nhood_sizes,
         row_batch_size=500, col_batch_size=100, num_gpus=1)
-    update_result_dict(result_d, ['decoded_gt_generated_', decoded_gt_generated_result])
+    update_result_dict(result_d, ['decoded_gt_prior_generated_', 
+                                  decoded_gt_prior_generated_result])
     
+    # compute model recall: model(gt) vs estimated training prior generated
+    decoded_gt_trained_prior_generated_result = iprd.knn_precision_recall_features(
+        reduced_decoded_gt_samples, 
+        reduced_generated_trained_prior_samples, 
+        nhood_sizes=nhood_sizes,
+        row_batch_size=500, col_batch_size=100, num_gpus=1)
+    update_result_dict(result_d, ['decoded_gt_trained_prior_generated_', 
+                                  decoded_gt_trained_prior_generated_result])
+    
+    subset_interventions = np.random.choice(
+          np.arange(num_recall_samples), size=num_interventions_per_latent_dim, replace=False)
     # Pick a latent dimension
     for dim in range(latent_dim):
       print('\n\n\n Computing the recall for latent dim ', dim)
       # randomly intervene several times
       agg_fix_one_vs_prior_generated_result = {'precision': [], 'recall': []}
-      agg_fix_one_vs_prior_decoded_gt_result = {'precision': [], 'recall': []}
-      agg_fix_all_but_one_vs_prior_generated_result = {'precision': [], 'recall': []}
-      agg_fix_all_but_one_vs_prior_decoded_gt_result =  {'precision': [], 'recall': []}
+      agg_fix_one_vs_trained_prior_generated_result = {'precision': [], 'recall': []}
+      agg_fix_one_vs_decoded_gt_result = {'precision': [], 'recall': []}
+      
+      agg_vary_one_vs_prior_generated_result = {'precision': [], 'recall': []}
+      agg_vary_one_vs_trained_prior_generated_result = {'precision': [], 'recall': []}
+      agg_vary_one_vs_decoded_gt_result =  {'precision': [], 'recall': []}
       
       for intervention in range(num_interventions_per_latent_dim):
+        inter_id = subset_interventions[intervention]
         print('n\n\n Intervention num', intervention)
-        # --- fix one, vary the rest
-        latent_intervention = np.random.normal()
-        fix_one_latent_from_prior_samples = np.copy(latent_prior_samples_np)
-        fix_one_latent_from_prior_samples[:, dim] = latent_intervention
-        # decode the samples and trasform them with the PCA
-        gen_fix_one_latent_from_prior_samples = decoder_fn(
-            fix_one_latent_from_prior_samples).reshape(num_recall_samples, -1)
-        reduced_gen_fix_one_latent_from_prior_samples = generated_pca.transform(
-            gen_fix_one_latent_from_prior_samples)
-        assert(reduced_gen_fix_one_latent_from_prior_samples.shape == \
-               reduced_generated_prior_samples.shape)
+        print(' Intervention row', inter_id)
         
-        # Calculate the relative recall
+        # --- fix one, vary the rest
+        latent_intervention = float(fixed_trained_prior_samples_np[inter_id, dim])
+        fix_one_latent_from_trained_prior_samples = np.copy(fixed_trained_prior_samples_np)
+        fix_one_latent_from_trained_prior_samples[:, dim] = latent_intervention
+        # decode the samples and trasform them with the PCA
+        gen_fix_one_latent_from_trained_prior_samples = decoder_fn(
+            fix_one_latent_from_trained_prior_samples).reshape(num_recall_samples, -1)
+        reduced_gen_fix_one_latent_from_trained_prior_samples = decoded_gt_pca.transform(
+            gen_fix_one_latent_from_trained_prior_samples)
+        assert(reduced_gen_fix_one_latent_from_trained_prior_samples.shape == \
+               reduced_generated_prior_samples.shape)
+
+        
+        # - Calculate the relative recall
+        # compare to the generated normal prior samples
         fix_one_vs_prior_generated_result = iprd.knn_precision_recall_features(
             reduced_generated_prior_samples, 
-            reduced_gen_fix_one_latent_from_prior_samples,
+            reduced_gen_fix_one_latent_from_trained_prior_samples,
             nhood_sizes=nhood_sizes,
             row_batch_size=500, col_batch_size=100, num_gpus=1)
+        print(agg_fix_one_vs_prior_generated_result)
+        print(fix_one_vs_prior_generated_result)
         agg_fix_one_vs_prior_generated_result = agg_recall_dict(
             agg_fix_one_vs_prior_generated_result, 
             fix_one_vs_prior_generated_result,
             intervention)
+        print(agg_fix_one_vs_prior_generated_result)
         
-        fix_one_vs_prior_decoded_gt_result = iprd.knn_precision_recall_features(
-            reduced_decoded_gt_samples, 
-            reduced_gen_fix_one_latent_from_prior_samples,
+        # compare to the generated trained prior samples
+        fix_one_vs_trained_prior_generated_result = iprd.knn_precision_recall_features(
+            reduced_generated_trained_prior_samples, 
+            reduced_gen_fix_one_latent_from_trained_prior_samples,
             nhood_sizes=nhood_sizes,
             row_batch_size=500, col_batch_size=100, num_gpus=1)
-        agg_fix_one_vs_prior_decoded_gt_result = agg_recall_dict(
-            agg_fix_one_vs_prior_decoded_gt_result, 
-            fix_one_vs_prior_decoded_gt_result,
+        agg_fix_one_vs_trained_prior_generated_result = agg_recall_dict(
+            agg_fix_one_vs_trained_prior_generated_result, 
+            fix_one_vs_trained_prior_generated_result,
+            intervention)
+        
+        # compare to the decoded gt 
+        fix_one_vs_decoded_gt_result = iprd.knn_precision_recall_features(
+            reduced_decoded_gt_samples, 
+            reduced_gen_fix_one_latent_from_trained_prior_samples,
+            nhood_sizes=nhood_sizes,
+            row_batch_size=500, col_batch_size=100, num_gpus=1)
+        agg_fix_one_vs_decoded_gt_result = agg_recall_dict(
+            agg_fix_one_vs_decoded_gt_result, 
+            fix_one_vs_decoded_gt_result,
             intervention)
         
         
         # --- vary one, fix the rest
-        dim_latent_prior_samples = np.copy(latent_prior_samples_np[:, dim])
-        fix_all_but_one_from_prior_samples = np.full(latent_shape, latent_intervention)
-        fix_all_but_one_from_prior_samples[:, dim] = dim_latent_prior_samples
+        latent_variation = np.copy(fixed_trained_prior_samples_np[:, dim])
+        print(latent_variation.shape, latent_variation)
+        vary_one_latent_from_trained_prior_samples = np.copy(
+            fixed_trained_prior_samples_np[inter_id]).reshape(1, latent_dim)
+        vary_one_latent_from_trained_prior_samples = np.full(
+            latent_shape, vary_one_latent_from_trained_prior_samples)
+        print(vary_one_latent_from_trained_prior_samples.shape)
+        print(vary_one_latent_from_trained_prior_samples)
+        
+        vary_one_latent_from_trained_prior_samples[:, dim] = latent_variation
+        print(vary_one_latent_from_trained_prior_samples.shape)
+        print(vary_one_latent_from_trained_prior_samples)
+        
         # decode the samples and trasform them with the PCA
-        gen_fix_all_but_one_latent_from_prior_samples = decoder_fn(
-            fix_all_but_one_from_prior_samples).reshape(num_recall_samples, -1)
-        reduced_gen_fix_all_but_one_latent_from_prior_samples = generated_pca.transform(
-            gen_fix_all_but_one_latent_from_prior_samples)
-        assert(reduced_gen_fix_all_but_one_latent_from_prior_samples.shape == \
+        gen_vary_one_latent_from_trained_prior_samples = decoder_fn(
+            vary_one_latent_from_trained_prior_samples).reshape(num_recall_samples, -1)
+        reduced_gen_vary_one_latent_from_trained_prior_samples = decoded_gt_pca.transform(
+            gen_vary_one_latent_from_trained_prior_samples)
+        assert(reduced_gen_vary_one_latent_from_trained_prior_samples.shape == \
                reduced_generated_prior_samples.shape)
         
-        # calculate the recall
-        fix_all_but_one_vs_prior_generated_result = iprd.knn_precision_recall_features(
+        # - Calculate the recall
+        # compare to the generated normal prior samples
+        vary_one_vs_prior_generated_result = iprd.knn_precision_recall_features(
             reduced_generated_prior_samples, 
-            reduced_gen_fix_all_but_one_latent_from_prior_samples,
+            reduced_gen_vary_one_latent_from_trained_prior_samples,
             nhood_sizes=nhood_sizes,
             row_batch_size=500, col_batch_size=100, num_gpus=1)
-        agg_fix_all_but_one_vs_prior_generated_result = agg_recall_dict(
-            agg_fix_all_but_one_vs_prior_generated_result, 
-            fix_all_but_one_vs_prior_generated_result,
+        print(agg_vary_one_vs_prior_generated_result)
+        print(vary_one_vs_prior_generated_result)
+        agg_vary_one_vs_prior_generated_result = agg_recall_dict(
+            agg_vary_one_vs_prior_generated_result, 
+            vary_one_vs_prior_generated_result,
+            intervention)
+        print(agg_vary_one_vs_prior_generated_result)
+        
+        # compare to the generated trained prior samples
+        vary_one_vs_trained_prior_generated_result = iprd.knn_precision_recall_features(
+            reduced_generated_trained_prior_samples, 
+            reduced_gen_vary_one_latent_from_trained_prior_samples,
+            nhood_sizes=nhood_sizes,
+            row_batch_size=500, col_batch_size=100, num_gpus=1)
+        agg_vary_one_vs_trained_prior_generated_result = agg_recall_dict(
+            agg_vary_one_vs_trained_prior_generated_result, 
+            vary_one_vs_trained_prior_generated_result,
             intervention)
         
-        fix_all_but_one_vs_prior_decoded_gt_result = iprd.knn_precision_recall_features(
+        # compare to the decoded gt 
+        vary_one_vs_decoded_gt_result = iprd.knn_precision_recall_features(
             reduced_decoded_gt_samples, 
-            reduced_gen_fix_all_but_one_latent_from_prior_samples,
+            reduced_gen_vary_one_latent_from_trained_prior_samples,
             nhood_sizes=nhood_sizes,
             row_batch_size=500, col_batch_size=100, num_gpus=1)
-        agg_fix_all_but_one_vs_prior_decoded_gt_result = agg_recall_dict(
-            agg_fix_all_but_one_vs_prior_decoded_gt_result, 
-            fix_all_but_one_vs_prior_decoded_gt_result,
+        agg_vary_one_vs_decoded_gt_result = agg_recall_dict(
+            agg_vary_one_vs_decoded_gt_result, 
+            vary_one_vs_decoded_gt_result,
             intervention)
+        
       update_result_dict_with_agg(
           result_d, 
           [str(dim) + '_fix_one_vs_prior_generated_', agg_fix_one_vs_prior_generated_result],
-          [str(dim) + '_fix_one_vs_prior_decoded_gt_', agg_fix_one_vs_prior_decoded_gt_result],
-          [str(dim) + '_fix_all_but_one_vs_prior_generated_', agg_fix_all_but_one_vs_prior_generated_result], 
-          [str(dim) + '_fix_all_but_one_vs_prior_decoded_gt_', agg_fix_all_but_one_vs_prior_decoded_gt_result])
+          [str(dim) + '_fix_one_vs_trained_prior_generated_', fix_one_vs_trained_prior_generated_result],
+          [str(dim) + '_fix_one_vs_decoded_gt_', agg_fix_one_vs_decoded_gt_result],
+          [str(dim) + '_vary_one_vs_prior_generated_', agg_vary_one_vs_prior_generated_result], 
+          [str(dim) + '_vary_one_vs_trained_prior_generated_', agg_vary_one_vs_trained_prior_generated_result],
+          [str(dim) + '_vary_one_vs_decoded_gt_', agg_vary_one_vs_decoded_gt_result])
+  print(result_d)
   return result_d
     
 
--- sample_efficiency/moria_copy.sh
+++ sample_efficiency/moria_copy.sh
@@ -2,21 +2,21 @@
 
 declare -a modelArr=(
 	"b8vae"
-	"vae"
-	"bvae"
-	"fvae"
-	"btcvae"
-	"annvae"
+	# "vae"
+	# "bvae"
+	# "fvae"
+	# "btcvae"
+	# "annvae"
 )
 
 declare -a datasetArr=(
 	"3dshapes_model_s100"
-	"3dshapes_model_s1000"
+	# "3dshapes_model_s1000"
 	"3dshapes_model_s10000"
-	"3dshapes_model_s50000"
-	"3dshapes_model_s100000"
-	"3dshapes_model_s150000"
-	"3dshapes_model_s250000"
+	# "3dshapes_model_s50000"
+	# "3dshapes_model_s100000"
+	# "3dshapes_model_s150000"
+	# "3dshapes_model_s250000"
 	)
 		
 declare -a seedArr=(1602 1201 1012 2805)
