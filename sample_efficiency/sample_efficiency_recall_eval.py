bb5773df6755302c4c141289f121b2f118779b52
petrapoklukar <petra.poklukar@gmail.com>
2020-07-06 19:51:34 +0200

    Testing new PCA eval

--- sample_efficiency/sample_efficiency_recall_eval.py
+++ sample_efficiency/sample_efficiency_recall_eval.py
@@ -83,6 +83,7 @@ def main_per_holdout(unused_argv):
       "evaluate_with_decodings.evaluation_fn = @recall_on_holdout",
       "evaluate_with_decodings.postprocess_fn = @mean_representation",
       "evaluate_with_decodings.random_seed = 0",
+      "evaluate_with_decodings.pca_components = [100, 500]",
       "dataset.name = '3dshapes_pca_holdout_s5000'",
       "recall_on_holdout.nhood_sizes = [3]",
       "recall_on_holdout.num_interventions_per_latent_dim = 1",
