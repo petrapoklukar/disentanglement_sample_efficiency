3621aa924a0fe7eec030767e01133f467da94afd
petrapoklukar <petra.poklukar@gmail.com>
2020-07-06 16:59:06 +0200

    Testing PCA generation

---
+++ disentanglement_lib/methods/unsupervised/pca.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 13:58:31 2020
+
+@author: petrapoklukar
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import gin.tf
+import os
+from disentanglement_lib.data.ground_truth import named_data
+import numpy as np
+import pickle
+from sklearn.decomposition import PCA
+
+def train_pca_with_gin(model_dir,
+                       overwrite=False,
+                       gin_config_files=None,
+                       gin_bindings=None):
+  """Trains a PCA based on the provided gin configuration.
+
+  This function will set the provided gin bindings, call the train() function
+  and clear the gin config. Please see train() for required gin bindings.
+
+  Args:
+    model_dir: String with path to directory where model output should be saved.
+    overwrite: Boolean indicating whether to overwrite output directory.
+    gin_config_files: List of gin config files to load.
+    gin_bindings: List of gin bindings to use.
+  """
+  if gin_config_files is None:
+    gin_config_files = []
+  if gin_bindings is None:
+    gin_bindings = []
+  gin.parse_config_files_and_bindings(gin_config_files, gin_bindings)
+  train_pca(model_dir, overwrite)
+  gin.clear_config()
+
+  
+@gin.configurable("train_pca", blacklist=["model_dir", "overwrite"])
+def train_pca(model_dir,
+              overwrite=False,
+              random_seed=gin.REQUIRED,
+              num_pca_components=gin.REQUIRED,
+              name="",
+              model_num=None):
+  """Trains the pca and saves it.
+
+  Args:
+    model_dir: String with path to directory where model output should be saved.
+    overwrite: Boolean indicating whether to overwrite output directory.
+    random_seed: Integer with random seed used for training.
+    num_pca_components: list with the number of pca components.
+    name: Optional string with name of the model (can be used to name models).
+    model_num: Optional integer with model number (can be used to identify
+      models).
+  """
+  # Obtain the datasets.
+  dataset_train, _ = named_data.get_named_ground_truth_data()
+
+  # Create a numpy random state. We will sample the random seeds for training
+  # and evaluation from this.
+  random_state = np.random.RandomState(random_seed)
+  
+  # Save the pca
+  pca_dir = os.path.join(model_dir, "pca")
+  for num_comp in num_pca_components:
+    pca = PCA(n_components=num_comp, random_state=random_state)
+    trained_pca = pca.fit(dataset_train.images)
+
+    pca_model_name = 'pca_{0}_{1}comp.pkl'.format(dataset_train.name, str(num_comp))
+    pca_export_path = os.path.join(pca_dir, pca_model_name)
+    with open(pca_export_path, 'wb') as f:
+      pickle.dump(trained_pca, f)
+    
+    
+    
+    
+    
+    
+  
---
+++ sample_efficiency/sample_efficiency_train_pca.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 13:54:26 2020
+
+@author: petrapoklukar
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import os
+from absl import app
+from absl import flags
+import sys
+sys.path.append('..')
+from disentanglement_lib.preprocessing import preprocess
+from disentanglement_lib.unsupervised import pca
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("model", None, "vae model to use")
+flags.DEFINE_string("dataset", None, "dataset to use")
+flags.DEFINE_boolean("overwrite", False,
+                     "Whether to overwrite output directory.")
+flags.DEFINE_integer("rng", 0,
+                     "random seed")
+
+
+def main(unused_argv):
+  base_path = "backbone"
+  model_path = os.path.join(base_path, "pca")
+  
+  print("\n\n*- Preprocessing '%s' \n\n" %(FLAGS.dataset))
+  preproces_gin_bindings = [
+            "dataset.name = '%s'" %(FLAGS.dataset),
+            "preprocess.preprocess_fn = @split_train_and_validation",
+            "split_train_and_validation.random_seed = %d" %(FLAGS.rng)
+      ]
+
+  preprocess.preprocess_with_gin(FLAGS.dataset,
+                                 FLAGS.model,
+                                 overwrite=FLAGS.overwrite,
+                                 gin_config_files=None,
+                                 gin_bindings=preproces_gin_bindings)
+  print("\n\n*- Preprocessing DONE \n\n")
+  
+  print("\n\n*- Training PCA.")
+  gin_bindings = [
+      "dataset.name = '%s'" %(FLAGS.dataset),
+      "train_pca.random_seed = 0",
+      "train_pca.num_pca_components = [10, 30]",#[100, 500, 1000, 2000, 4000]",
+  ]
+  pca.train_pca_with_gin(
+      model_path, FLAGS.overwrite, gin_bindings=gin_bindings)
+  
+  preprocess.destroy_train_and_validation_splits(
+      FLAGS.dataset + '_' + FLAGS.model + '_' + str(FLAGS.rng))
+  print("\n\n*- Training COMPLETED \n\n")
+
+
+if __name__ == "__main__":
+  app.run(main)
